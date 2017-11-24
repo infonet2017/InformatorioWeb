b8df27318213db4fee31a9e40fa9f781b9246ab6
Eve Bejarano <evebejarano.eb@gmail.com>
2017-11-24 15:23:58 -0300

    Add Files

---
+++ PruebaFiles/PruebaFiles/Models/SupportContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PruebaFiles.Models
+{
+    public class SupportContext : DbContext
+    {
+        public SupportContext()
+            : base(nameOrConnectionString: "name=FilesDB")
+        {
+        }
+        public DbSet<Support> Supports { get; set; }
+        public DbSet<FileDetail> FileDetails { get; set; }
+    }
+}
